2d5e26a96e0f3edfbe245453069f59a33cfabb59
Samuel <sjoseph1848@gmail.com>
2019-04-04 09:39:12 -0500

    Added controlled forms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import Contacts from './components/Contacts';
-import Header from './components/Header';
+import Contacts from './components/contacts/Contacts';
+import Header from './components/layout/Header';
+import AddContact from './components/contacts/AddContact';
+
+import {Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -8,12 +11,15 @@ import './App.css';
 class App extends Component {
   render() { 
     return (
+      <Provider>
       <div className="App">
         <Header branding="Contact Manager"/>
         <div className="container">
+          <AddContact />
         <Contacts />
         </div>
       </div>
+      </Provider>
     );
   }
 }
